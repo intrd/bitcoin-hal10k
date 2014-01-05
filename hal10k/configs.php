84c420ca17e889eb4dab5e0d0f35eaded20205ea
intrd <contact@dann.com.br>
2014-01-05 11:22:48 -0200

    v2.1 beta5

--- hal10k/configs.php
+++ hal10k/configs.php
@@ -34,22 +34,22 @@
 	$fake_index=$datadir."fake_index.txt"; //Filename of backtesting index buffer
 	
 	/* MTGox app data. If you do not have access to the API gox, visit: https://www.mtgox.com/security and create your key (with read/write) */
-	$gox["app_id"]="XXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXX";
-	$gox["app_secret"]="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+	$gox["app_id"]="xxxxxxxxxxxxxxxxxxxxxxxx";
+	$gox["app_secret"]="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 	$login="intrd";
 
 	/* Custom trading algoritm parameters */
 	$percentual=0.45; //Minimum percentage of profit on the purchase (Use the current fee applied by MtGox);
 	$bidfee=0.45; //Just for simulation purposes (Use the current fee applied by MtGox);
-	$up_diff=0; //(sell) profit points (in USD) above the purchase price;
-	$up_diff_inv=990; //(sell) stop loss (in USD) below the purchase price;
-	$down_diff=0; //(buy) profit points (in USD) below the selling price;
+	$up_diff=10; //(sell) profit points (in USD) above the purchase price;
+	$up_diff_inv=999; //(sell) stop loss (in USD) below the purchase price;
+	$down_diff=10; //(buy) profit points (in USD) below the selling price;
 	$down_diff_inv=999; //(buy) stop loss (in USD) above the selling price;
 	$secure_ticker=300.99500; //Security value that prevents the bot to make sales below a certain value;
 	$emacross=true; //Turn on EMA crossover method (if emacross=true, Simple Market Direction Method automatically is turned off);
-	$emaShort=10; //EMA short period(in minutes because you are using $interval=60;) for EMA crossover method;
-	$emaLong=25; //EMA long period(in minutes because you are using $interval=60;) for EMA crossover method;
-	$emaDiff=0; //EMA difference between short and long crossover;
+	$emaShort=20; //EMA short period(in minutes because you are using $interval=60;) for EMA crossover method;
+	$emaLong=44; //EMA long period(in minutes because you are using $interval=60;) for EMA crossover method;
+	$emaDiff=5; //EMA difference between short and long crossover;
 	$last_two_orders=false; //Base next move on the last two transactions value;
 	$interval=60; //Bot loop interval (in seconds);
 	$timeout=80; //Timeout in seconds for completion of the bid/ask;
@@ -63,7 +63,7 @@
 	/* Backtesting and Paper trading */
 	$fake=true; //Turns on/off simulation (backtesting);
 	$paper=false; //Paper trading is simulation with Real Live Tickers but w/ fake money balance. (If Paper trading is true, $fake needs to be defined to "true");
-	$fake_btc_balance="0.03161308"; //Initial amount of BTC for the simulation;
+	$fake_btc_balance="0.04161308"; //Initial amount of BTC for the simulation;
 	$fake_btc_usd_buyedprice="707.00000"; //Initial purchase price of BTC (ATENTION: you need to set this value for Backtesting and Paper trading);
 	$fake_datetime_of_firstbid="2014-01-01 00:00:00"; //Initial datetime (same as first line of $fakegox_tickers file below)
 	$fakegox_tickers=$datadir."fakegox_tickers(24.12-26.12).txt"; //Access http://bitcoincharts.com/charts/mtgoxUSD, set the period (must be a period that supports interval 1min), click Raw Data, copy/paste the contents of the table in a TXT file and replaces tabulations by "," comma);
@@ -91,7 +91,6 @@
 		wfilenew($lastfile_clean,$default_data);
 	}
 
-echo "\r\nWhen the bot is running, access hal10k/index.php on your browser to open the HTTP Control Panel"; //COMMENT THIS LINE AT FIRST RUN 
-echo "\r\nFirst running? edit these files: configs.php e o hal10k.bat."; sleep(100); //COMMENT THIS LINE AT FIRST RUN 
-
+echo "\r\n### Access hal10k/index.php on your browser to open the HTTP Control Panel."; //COMMENT THIS LINE AT FIRST RUN 
+echo "\r\n### First running? edit these files: configs.php e o hal10k.bat, and comment '//' this line."; sleep(100); //COMMENT THIS LINE AT FIRST RUN 
 ?>
